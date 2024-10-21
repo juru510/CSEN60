78b8fffd4991088fb37dc83576ce5e801cc1a7f1
juru510 <147952345+juru510@users.noreply.github.com>
2024-10-21 11:31:48 -0700

    i couldnt type the index.html stuff so the js doesnt run i will look at class recording to fix

--- week5/index.js
+++ week5/index.js
@@ -1,1 +1,16 @@
-console.log("Hello world")
+// console.log("Hello world")
+// alert("Hello world");
+window.document.write(<p>welcome to csen60!</p>);
+
+// document.append(<p>this should come after the h1</p>)
+
+/*
+    Function to calculate the sum of two numbers.
+    Inputs: a, b - numbers to be added.
+    Output: returns the sum of a and b.
+*/
+
+// TODO: fix this function
+function add(a, b) {
+    return a + b;
+}
