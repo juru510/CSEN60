b90767ebaba0847b69996b55ba8494aed22c1792
juru510 <147952345+juru510@users.noreply.github.com>
2024-10-23 11:34:36 -0700

    learned about variables and operators

--- week5/index.js
+++ week5/index.js
@@ -1,6 +1,6 @@
 // console.log("Hello world")
 // alert("Hello world");
-window.document.write(<p>welcome to csen60!</p>);
+// window.document.write("<p>welcome to csen60!</p>");
 
 // document.append(<p>this should come after the h1</p>)
 
@@ -14,3 +14,55 @@ window.document.write(<p>welcome to csen60!</p>);
 function add(a, b) {
     return a + b;
 }
+
+let x = 100
+// const x => declaration
+// x = 2 => initialization
+if (true) {
+    let x = 200; // Same variable!
+}
+console.log(x); // Outputs: 200
+
+
+
+let y 
+y = null
+let z = 0
+console.log(y,z)
+
+let greeting = "Hello, world!";
+let response = `The time is ${new Date().getHours()}`;
+console.log(response)
+
+const age = 20
+if (age > 18){
+        console.log("you are of age")
+} 
+else{
+    console.log(" you arent of age, please wait ${18-age} years")
+}
+let isAvailable = true;
+let isOverAge = (age > 18);
+
+console.log("\n", isOverAge)
+
+let person = {
+    name: "john",
+    age: 30,
+    isStudent: false
+};
+
+console.log(person["age"])
+
+console.log(typeof(person))
+
+alert("this is an important message")
+
+
+// Strict equality
+console.log(3 === '3'); // false
+
+// Loose equality
+console.log(3 == '3'); // true
+
+let x=2
