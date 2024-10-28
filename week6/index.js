14f4f03533d7e7d34c5f068f09818ab7b0d94930
juru510 <147952345+juru510@users.noreply.github.com>
2024-10-28 11:35:11 -0700

    create addToDo Function

---
+++ week6/index.js
@@ -0,0 +1,16 @@
+
+
+// takes input yap //
+function addToDO() {
+    console.log("button was clicked")
+    const input = document.getElementById("list-input")
+    console.log("input.value =", input.value)    
+
+    const ul = document.querySelector("ul")
+    // const list = lists[0]
+    console.log(ul)
+
+const li = document.createElement("li")
+li.textcontent = input.value
+ul.appendChild(li)
+}
